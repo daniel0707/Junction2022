7c3b7bd59b68c68da6b79565d1077b4f59c66a6b
Ville Lohkovuori <villelohkovuori1@gmail.com>
2022-11-04 23:18:35 +0200

    add fields to GCList and Card

--- frontend/src/components/GarbageCenterList.jsx
+++ frontend/src/components/GarbageCenterList.jsx
@@ -1,8 +1,17 @@
+import GarbageCenterCard from "./GarbageCenterCard"
 
 export default function GarbageCenterList() {
 
   return (<div>
     <h1>Click on a garbage center to choose it</h1>
-    {props.children}
+    {props.children.map(gc => {
+      return <GarbageCenterCard
+        name={gc.name}
+        address={gc.address}
+        deliveryPrice={gc.price}
+        eta={gc.eta}
+        handleClick={gc.handleClick}
+      />
+    })}
   </div>)
 }
