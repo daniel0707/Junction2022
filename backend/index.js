ef76689d335a96a866d630bf4f2c19c5502984af
Daniel Vahla <vahla.daniel@gmail.com>
2022-11-05 17:29:55 +0200

    switch find body details

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { Pool, Client } = require("pg");
+const { Pool } = require("pg");
 const fs = require("fs");
 const axios = require("axios");
 const parse = require("pg-connection-string").parse;
@@ -12,8 +12,8 @@ const api_key = process.env.WOLT_TOKEN || "";
 const MERCHANT_ID = process.env.MERCHANT_ID || "";
 const GMAP_TOKEN = process.env.GMAP_TOKEN;
 
-if (!GMAP_TOKEN){
-  throw "No google map token"
+if (!GMAP_TOKEN) {
+  throw "No google map token";
 }
 
 if (postgresqlUri == "") {
@@ -51,8 +51,8 @@ app.get("/categories", (req, res, next) => {
 
 app.post("/order/find", (req, res, next) => {
   const material_ids = req.body.materials;
-  const coord = req.body.location.coordinates;
-  const address = req.body.location;
+  const coord = req.body.coordinates;
+  const address = req.body.address;
   const querystring = `SELECT r.id, r.address, r.spot_name, r.municipality, r.postal_code, ST_DISTANCE(r.coordinates,'SRID=4326;POINT(${
     coord.lng
   } ${coord.lat})'::geography) as distance
@@ -95,12 +95,12 @@ const find_price = async (rows, address) => {
     let req_body = {
       pickup: {
         location: {
-          formatted_address: `${address.street}, ${address.postal_code} ${address.city}`,
+          formatted_address: address,
         },
       },
       dropoff: {
         location: {
-          formatted_address: `${row.address}, ${row.postal_code} ${row.municipality}`,
+          formatted_address: `${row.address}, ${fix_postal_code(row.postal_code)} ${row.municipality}`,
         },
       },
     };
@@ -117,63 +117,147 @@ const find_price = async (rows, address) => {
   return res_arr;
 };
 
-app.post("/order/submit", (req, res, next) => {
-  const pickup_location = req.body.pickup
-  const pickup = {location:pickup_location,
-  contact_details:{
-    "name": "John Wolt",
-    "phone_number":"+358456456456",
-    "send_tracking_link_sms": false
-  }}
-  const dropoff = {
-    "location": {
-      "formatted_address": req.body.dropoff
-    },
-    "contact_details": {
-      "name": "John Wolt's wife",
-      "phone_number": "+358123456789",
-      "send_tracking_link_sms": false
-    },
-    "comment": "Leave at the door, please"
+app.post("/order/submit", async (req, res, next) => {
+  const dropoff_details = await get_recyclepoint_details(req.body.dropoff)
+  if (!dropoff_details){
+    throw "Could not find dropoff with that ID"
   }
+
+ /* 
+ req.body = { 
+  dropoff:{
+    formatted_adress: "Street, postcode address"
+  },
+  pickup: "recyclepoint_ID"
+  content:[
+    {
+      "count": 1,
+      "description": material_name
+      "identifier": material_code
+      "tags": [material_name]
+    }
+  ]
+ }
+ */
+  const dropoff = {
+    location: {
+      formatted_address: `${dropoff_details.address}, ${fix_postal_code(dropoff_details.postal_code)} ${dropoff_details.municipality}`
+    },
+    contact_details: {
+      name: "John Wolt",
+      phone_number: "+358456456456",
+      send_tracking_link_sms: false,
+    },
+  };
+  const pickup = {
+    location: {
+      formatted_address: req.body.pickup,
+    },
+    contact_details: {
+      name: "John Wolt's wife",
+      phone_number: "+358123456789",
+      send_tracking_link_sms: false,
+    },
+  };
+  const extras = {
+    customer_support: {
+      email: "should be an email",
+      phone_number: "should be phone nr",
+      url: "should be url",
+    },
+    is_no_contact: true,
+    min_preparation_time_minutes: 0,
+    tips: [
+      {
+        type: "pre_delivery_courier_tip",
+        price: {
+          amount: 0,
+          currency: "string",
+        },
+      },
+    ],
+  };
+  const payload = {...extras,"pickup":pickup,"dropoff":dropoff}
+  const wolt_order_resp = await wolt_order(payload)
+  console.log(wolt_order_resp.data)
+
 });
 
-
-const baseGmapUrl = 'https://maps.googleapis.com/maps/api/geocode/json?';
-const  geocodeRequestConstuctor = (address) => {
-  return `${baseGmapUrl}address=${encodeURI(address)}&key=${GMAP_TOKEN}`
+const wolt_order = async (body)=>{
+  const order_url = `https://daas-public-api.development.dev.woltapi.com/merchants/${MERCHANT_ID}/delivery-order`;
+  let req_config = {
+    headers: {
+      Authorization: `Bearer ${api_key}`,
+    },
+  };
+  return await axios.post(order_url,body,req_config)
 }
 
+const get_recyclepoint_details = async (id) =>{
+  pool.connect();
+  pool.query(`SELECT * FROM recyclepoint WHERE recyclepoint.id=${id}`, [], (err, result) => {
+    if (err) {
+      console.log(err);
+      return []
+    } else {
+      return result.rows[0]
+    }
+  });
+}
+//get_recyclepoint_details(1218666240)
+const baseGmapUrl = "https://maps.googleapis.com/maps/api/geocode/json?";
+const geocodeRequestConstuctor = (address) => {
+  return `${baseGmapUrl}address=${encodeURI(address)}&key=${GMAP_TOKEN}`;
+};
+
 const reverseGeocodeRequestConstructor = (coord) => {
-  return `${baseGmapUrl}address=${coord.lat},${coord.lng}&key=${GMAP_TOKEN}`
+  return `${baseGmapUrl}address=${coord.lat},${coord.lng}&key=${GMAP_TOKEN}`;
+};
+
+const fix_postal_code = (postal_code)=>{
+  if(`${postal_code}`.length==3){
+    return `00${postal_code}`
+  }
+  if(`${postal_code}`.length==4){
+    return `0${postal_code}`
+  }
+  return `${postal_code}`
 }
 
 app.get("/gmapproxy/geocode", (req, res, next) => {
   const address = req.body.address;
-  axios.get(geocodeRequestConstuctor(address)).then((gres)=>{
-    console.log('Geocode response', gres.data.results[0].geometry.location);
-    const lnglat = gres?.data?.results[0]?.geometry?.location
-    res.send({coord: lnglat});
-  }, (err)=>{
-    console.log('Geocodee error', err);
-  })
-})
+  axios.get(geocodeRequestConstuctor(address)).then(
+    (gres) => {
+      console.log("Geocode response", gres.data.results[0].geometry.location);
+      const lnglat = gres?.data?.results[0]?.geometry?.location;
+      res.send({ coord: lnglat });
+    },
+    (err) => {
+      console.log("Geocodee error", err);
+    }
+  );
+});
 
 app.get("/gmapproxy/reversegeocode", (req, res, next) => {
   const coord = req.body.coord;
-  axios.get(reverseGeocodeRequestConstructor(coord)).then((gres) =>{
-    console.log('Reverse geolocation ', gres.data);
-    const address = gres?.data?.results?.[0]?.formatted_address
-    res.send({address: address, fullData: gres?.data?.results?.[0]?.address_components});
-  }, (err) => {
-    console.log('Reverse geolocation error ', err);
-  })
-})
+  axios.get(reverseGeocodeRequestConstructor(coord)).then(
+    (gres) => {
+      console.log("Reverse geolocation ", gres.data);
+      const address = gres?.data?.results?.[0]?.formatted_address;
+      res.send({
+        address: address,
+        fullData: gres?.data?.results?.[0]?.address_components,
+      });
+    },
+    (err) => {
+      console.log("Reverse geolocation error ", err);
+    }
+  );
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
